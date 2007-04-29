98b13b2c498b2f0315c1088de4a680b7ceda5ee5
mats.palmgren@bredband.net <mats.palmgren@bredband.net>
2007-04-29 15:24:59 -0700

    Create a rendering context before calling GetPrefSize and friends and assert in those methods that we have one. b=379090 r+sr=dbaron

--- layout/xul/base/src/nsBox.cpp
+++ layout/xul/base/src/nsBox.cpp
@@ -471,6 +471,8 @@ nsBox::GetLayoutManager(nsIBoxLayout** aLayout)
 nsSize
 nsBox::GetPrefSize(nsBoxLayoutState& aState)
 {
+  NS_ASSERTION(aState.GetRenderingContext(), "must have rendering context");
+
   nsSize pref(0,0);
   DISPLAY_PREF_SIZE(this, pref);
 
@@ -490,6 +492,8 @@ nsBox::GetPrefSize(nsBoxLayoutState& aState)
 nsSize
 nsBox::GetMinSize(nsBoxLayoutState& aState)
 {
+  NS_ASSERTION(aState.GetRenderingContext(), "must have rendering context");
+
   nsSize min(0,0);
   DISPLAY_MIN_SIZE(this, min);
 
@@ -510,6 +514,8 @@ nsBox::GetMinSizeForScrollArea(nsBoxLayoutState& aBoxLayoutState)
 nsSize
 nsBox::GetMaxSize(nsBoxLayoutState& aState)
 {
+  NS_ASSERTION(aState.GetRenderingContext(), "must have rendering context");
+
   nsSize max(NS_INTRINSICSIZE, NS_INTRINSICSIZE);
   DISPLAY_MAX_SIZE(this, max);
 
@@ -562,6 +568,8 @@ nsBox::IsCollapsed(nsBoxLayoutState& aState)
 nsresult
 nsIFrame::Layout(nsBoxLayoutState& aState)
 {
+  NS_ASSERTION(aState.GetRenderingContext(), "must have rendering context");
+
   nsBox *box = NS_STATIC_CAST(nsBox*, this);
   DISPLAY_LAYOUT(box);
 
